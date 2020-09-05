2c4019063f16cd0ef843a7687a5b3a2e8513bcea
ritchytitchy <Richard58983@gmail.com>
2020-09-05 03:23:39 -0400

    Added dropdown menu
    Added a drop down that populates from selectable skills

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text textbox;
+    public static int skilllvl;
     void Start()
-    {
-        
-    }
+    { 
+    var changeskill = transform.GetComponent<Dropdown>();
+ 
+     changeskill.options.Clear();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    List<string> items = new List<string>();
+
+
+            skilllvl = BlackSmithingSkill.blacksmithlvl;
+        if ( skilllvl >= 1)
+        {
+            items.Add("Blacksmithing");
+        }
+        skilllvl = FarmingSkill.farminglvl;
+        if (skilllvl >= 1)
+        {
+             items.Add("Farming");
+        }
+        foreach(var item in items)
+{
+    changeskill.options.Add(new Dropdown.OptionData() { text = item });
+}
+        DropdownItemSelected(changeskill);
+        changeskill.onValueChanged.AddListener(delegate { DropdownItemSelected(changeskill); });
     }
+        void DropdownItemSelected(Dropdown changeskill)
+{
+     int index = changeskill.value;
+    textbox.text = changeskill.options[index].text;
+}
 }
